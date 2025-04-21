94f271ecc8c9e85aa7b491ea62739d8738fc2a03
Auri Gabriel <ririmello.a@gmail.com>
2025-04-21 14:22:29 -0300

    add documentation

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,3 +1,5 @@
+// src/routes/movie.routes.ts
+
 import { Router } from 'express'
 import {
   getAllMovies,
@@ -6,38 +8,96 @@ import {
   deleteMovie,
 } from '../controllers/movie.controller'
 
-const router = Router();
+const router = Router()
 
 /**
  * @openapi
  * /api/movies:
  *   get:
- *     summary: Get all movies
+ *     summary: Retrieve a list of movies
+ *     tags:
+ *       - Movies
  *     responses:
  *       200:
  *         description: A list of movies
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Movie'
  */
-router.get('/', getAllMovies);
+router.get('/', getAllMovies)
 
 /**
  * @openapi
  * /api/movies/{id}:
  *   get:
- *     summary: Get a movie by ID
+ *     summary: Get a single movie by ID
+ *     tags:
+ *       - Movies
  *     parameters:
- *       - name: id
- *         in: path
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
  *         required: true
+ *         description: The movie ID
  *     responses:
  *       200:
- *         description: A movie
+ *         description: A single movie
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Movie'
  *       404:
  *         description: Movie not found
  */
-router.get('/:id', getMovieById);
+router.get('/:id', getMovieById)
 
-router.post('/', createMovie);
+/**
+ * @openapi
+ * /api/movies:
+ *   post:
+ *     summary: Create a new movie
+ *     tags:
+ *       - Movies
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/MovieInput'
+ *     responses:
+ *       201:
+ *         description: Movie created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Movie'
+ */
+router.post('/', createMovie)
 
-router.delete('/:id', deleteMovie);
+/**
+ * @openapi
+ * /api/movies/{id}:
+ *   delete:
+ *     summary: Delete a movie by ID
+ *     tags:
+ *       - Movies
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The movie ID
+ *     responses:
+ *       204:
+ *         description: Movie deleted successfully
+ *       404:
+ *         description: Movie not found
+ */
+router.delete('/:id', deleteMovie)
 
 export default router
